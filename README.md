6c36faae04f5e975ac835936ec3f9e043acbdc9f
developper <developper@sutetchikurieitibuchimuAnoMacBook-Pro.local>
2019-11-05 17:03:42 +0900

    fix: * PixelWindowコンポーネントをexapleへ移動 * README.mdに雑なメモを追加

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# 1. Reactプロジェクト作成しとく
+# 1. React プロジェクト作成しとく
 
 ex)
+
 ```
 create-react-app your_react_project
 ```
 
-# 2. package.jsonに追加
+# 2. package.json に追加
 
 ```
 cd your_react_project
@@ -44,10 +45,105 @@ const YourComponent = () => (
 export default App;
 ```
 
-
 # for Component Develop
 
 ```
 npm install
 npm run demoStart
 ```
+
+# API メモ
+
+## Components
+
+- `ReactPixelStreaming`コンポーネント: WebRtc の接続管理や UE4 との疎通用のメソッド/プロパティを管理するラッパーコンポーネント。
+- `PixelWindow`コンポーネント: 実際に WebRTC の Video Stream がレンダリングされるコンポーネント。ReactPixelStreaming の子孫コンポーネントとして、context を受け取る必要がある。
+
+## Attributes
+
+### ReactPixelStreaming
+
+`ReactPixelStreaming`コンポーネントは以下の Attribute を受け取ります。
+
+```
+<ReactPixelStreaming
+  webRtcHost="${HOST}:${PORT}"
+  pixelStreamingResponseEvents={[]}
+>
+```
+
+- webRtcHost<Strings>: WebRTC のバックエンドを渡します。
+- pixelStreamingResponseEvents<Array>: UE4 で responsePixelStreaming が発行された時の処理を渡します。
+
+```
+ [{name: "handler", handler: function(response){
+   // some action
+ } ]
+```
+
+### PixelWindow
+
+`PixelWindow`コンポーネントは context を介して以下の値を渡す必要があります。
+
+```
+<PixelStreamingContext.Consumer>{
+  context=>{
+    <PixelWindow
+      load={context.load}
+      actions={context.actions}
+      connect={context.connect}
+      host={context.webRtcHost}
+      setVideoAspectRatio={context.actions.setVideoAspectRatio}
+      setPlayerAspectRatio={context.actions.setPlayerAspectRatio}
+    />
+  }
+}
+</PixelStreamingContext.Consumer>
+```
+
+### CustomComponent と context
+
+`PixelStreamingContext.Consumer`経由で子コンポーネントから`ReactPixelStreaming`のプロパティやメソッド等のコンテキストにアクセスできます。
+
+* videoAspectRatio: Video Streamのアスペクト比,
+* videoRes: Video Streamの幅/高さ
+  * width
+  * haight
+* responseEventListeners: 登録済みのpixelStreamingResponseイベントリスナー
+* addResponseEventListener: pixelStreamingResponseイベントリスナーの登録
+* removeResponseEventListener: pixelStreamingResponseイベントリスナーの削除
+* emitter: WebRTC経由でコマンドを送信する(UE4でPixelStreamingInputEventが発火する)
+  * emitter(context.webRtcPlayerObj).emitUIInteraction
+  * emitter(context.webRtcPlayerObj).emitMouseDown
+  * emitter(context.webRtcPlayerObj).emitMouseUp
+  * emitter(context.webRtcPlayerObj).emitMouseMove
+  * emitter(context.webRtcPlayerObj).emitMouseWheel
+  * emitter(context.webRtcPlayerObj).sendInputData
+  * emitter(context.webRtcPlayerObj).emitUIInteraction
+  * emitter(context.webRtcPlayerObj).emitCommand
+  * emitter(context.webRtcPlayerObj).emitDescriptor
+* controlScheme: マウスの制御オプション
+* suppressBrowserKeys: ファンクションキーの制御オプション
+* fakeMouseWithTouches: タッチスクリーンの制御オプション
+* webrtcState: 
+* webRtcPlayerObj: インスタンス課されたwebrtcオブジェクト
+* webRtcPlayer: webrtcコンストラクタ
+* connect: 
+* clientConfig: webrtcの接続情報 
+* socket: インスタンス化されたwsオブジェクト
+* aggregatedStats<Array>: webRtcPlayerObjが収集したwebrtc統計(interval: 1sec)
+  * avgBitrate : 平均ビットレート(Kbps)
+  * highBitrate: 最大ビットレート(Kbps)
+  * bitrate: ビットレート(Kbps)
+  * bytesReceived: 受信データ量(bytes)
+  * framerate: FPS
+  * hightFramerate: 最大FPS
+  * lowFramerate: 最小FPS
+* actions: 基本的に気にしなくて良い
+  * updateWebRTCStat: 
+  * updateClientConfig:
+  * updateSocket: 
+  * setVideoAspectRatio: 
+  * addLatestStats: 
+  * setWebRTCPlayerObj: 
+* load:
--- src/ReactPixelStreaming.js
+++ src/ReactPixelStreaming.js
@@ -1,29 +1,8 @@
 import React, { Component } from "react";
-import PixelWindow from "./PixelWindow";
 import PixelStreamingClient from "./lib/pixel-streaming-client";
 import PixelStreamingContext from "./lib/pixel-streaming-context";
-import { Rnd } from "react-rnd";
 import "./lib/emitter";
-import {
-  Link,
-  Paper,
-  Button,
-  AppBar,
-  List,
-  Radio,
-  Container,
-  FormControlLabel,
-  Slider,
-  Typography,
-  Fab,
-  Toolbar,
-  IconButton,
-} from "@material-ui/core";
 
-import { Fullscreen, FullscreenExit } from "@material-ui/icons";
-
-import QRCode from "qrcode.react";
-import { Line } from "react-chartjs-2";
 
 import emitter from "./lib/emitter";
 
@@ -38,14 +17,10 @@ export default class ReactPixelStreaming extends Component {
       removeResponseEventListener:
         PixelStreamingClient.removeResponseEventListener,
       emitter: emitter,
-      // emitCommand: PixelStreamingClient.emitCommand,
-      // emitDescriptor: PixelStreamingClient.emitDescriptor,
-      // emitUIInteraction: PixelStreamingClient.emitUIInteraction,
       controlScheme: PixelStreamingClient.controlScheme,
       suppressBrowserKeys: PixelStreamingClient.suppressBrowserKeys,
       fakeMouseWithTouches: PixelStreamingClient.fakeMouseWithTouches,
       webrtcState: "",
-      // webRtcPlayerObjをstate化
       webRtcPlayerObj: {}, //PixelStreamingClient.webRtcPlayerObj,
       webRtcPlayer: PixelStreamingClient.webRtcPlayer,
       connect: PixelStreamingClient.connect,
@@ -61,31 +36,6 @@ export default class ReactPixelStreaming extends Component {
         addLatestStats: this.addLatestStats,
         setWebRTCPlayerObj: this.setWebRTCPlayerObj
       },
-      // for Rnd
-      // x/yを初期化しとかないとdragが計算できない
-      x: 30,
-      y: 30,
-      x2: 950,
-      y2: 30,
-      x3: 30,
-      y3: 600,
-      x4: 650,
-      y4: 645,
-      x5: 400,
-      y5: 400,
-      width: 900,
-      // TODO: maxSize/ratio
-      disableDragging: false,
-      enableResizing: {
-        bottom: true,
-        bottomLeft: true,
-        bottomRight: true,
-        left: true,
-        right: true,
-        top: true,
-        topLeft: true,
-        topRight: true
-      },
       // TODO: video/playerAspect
       //  var playerAspectRatio = playerElement.clientHeight / playerElement.clientWidth; // We want to keep the video ratio correct for the video stream
       //  var videoAspectRatio = videoElement.videoHeight / videoElement.videoWidth;
@@ -93,21 +43,18 @@ export default class ReactPixelStreaming extends Component {
       videoAspectRatio: 1,
       playerRes: {},
       videoRes: {},
-      infomation: ""
     };
 
-    // 雑にresponse受け取る
-    PixelStreamingClient.addResponseEventListener("handle", this.setInfomation);
+    // eventの登録
+    props.pixelStreamingResponseEvents.forEach((event)=>{
+      PixelStreamingClient.addResponseEventListener(event.name, event.handler.bind(this));
+    })
   }
 
   setWebRTCPlayerObj = player => {
     this.setState({
       webRtcPlayerObj: player
     });
-  };
-
-  setInfomation = info => {
-    this.setState({ infomation: info });
   };
 
   addLatestStats = (stats, videoElement) => {
@@ -187,404 +134,15 @@ export default class ReactPixelStreaming extends Component {
     });
   };
 
-  componentDidMount() {
-    //        this.state.addResponseEventListener("handle_responses", this.props.handler);
-    //        console.log("atached handler");
-  }
-
-  componentWillUnmount() {
-    //        this.state.removeResponseEventListener("handle_responses");
-    //        console.log("removed handler");
-  }
-
   render() {
-    const c = this.state.aggregatedStats[this.state.aggregatedStats.length - 1];
     return (
       <PixelStreamingContext.Provider value={this.state}>
         <div style={this.props.style}>
-          <PixelStreamingContext.Consumer>
-            {context => (
-              <div>
-                <Rnd
-                  size={{ width: this.state.width, height: this.state.height }}
-                  position={{ x: this.state.x, y: this.state.y }}
-                  onDragStop={(e, d) => {
-                    this.setState({ x: d.x, y: d.y });
-                  }}
-                  onResizeStop={(e, direction, ref, delta, position) => {
-                    this.setState({
-                      width: ref.style.width,
-                      height: ref.style.height,
-                      ...position
-                    });
-                  }}
-                  disableDragging={this.state.disableDragging}
-                >
-                  <Paper>
-                    {/* TODO: 最小化、最大化、オリジナルサイズボタンを追加 */}
-                    <AppBar style={{ position: "relative", padding: 10 }}>
-                      <Toolbar>
-                        <Typography style={{"flexGrow": 1}}>
-                        Streaming Window
-                        </Typography>
-                        <IconButton color="inherit" onClick={()=>{this.setState({width: "100vw"})}}>
-                          <Fullscreen />
-                        </IconButton>
-                        <IconButton color="inherit" onClick={()=>{this.setState({width: "900"})}}>
-                          <FullscreenExit />
-                        </IconButton>
-                      </Toolbar>
-                    </AppBar>
-                    <PixelWindow
-                      load={context.load}
-                      actions={context.actions}
-                      connect={context.connect}
-                      host={this.props.webRtcHost}
-                      setDisableDragging={this.setDisableDragging}
-                      setEnableResizing={this.setEnableResizing}
-                      setVideoAspectRatio={this.setVideoAspectRatio}
-                      setPlayerAspectRatio={this.setPlayerAspectRatio}
-                    />
-                  </Paper>
-                </Rnd>
-                <Rnd
-                  size={{ width: "300", height: "600" }}
-                  position={{ x: this.state.x2, y: this.state.y2 }}
-                  onDragStop={(e, d) => {
-                    this.setState({ x2: d.x, y2: d.y });
-                  }}
-                  onResizeStop={(e, direction, ref, delta, position) => {
-                    this.setState({
-                      width: ref.style.width,
-                      height: ref.style.height,
-                      ...position
-                    });
-                  }}
-                  enableResizing={{}}
-                >
-                  <Paper shadow={2} style={{ position: "relative" }}>
-                    <AppBar style={{ position: "relative", padding: 10 }}>
-                      統計テーブル
-                    </AppBar>
-                    <Paper style={{ padding: 30 }}>
-                      <List>
-                        ウィンドウレシオ:{" "}
-                        {this.state.playerAspectRatio.toFixed(3)}
-                      </List>
-                      <List>
-                        ウインドウサイズ: {this.state.playerRes.width}/
-                        {this.state.playerRes.height}
-                      </List>
-                      <List>
-                        ビデオレシオ: {this.state.videoAspectRatio.toFixed(3)}
-                      </List>
 
-                      <List>
-                        ビデオ解像度: {this.state.videoRes.width}/
-                        {this.state.videoRes.height}
-                      </List>
-                      {c ? (
-                        <div>
-                          <List>
-                            平均ビットレート: {(c.avgBitrate / 1024).toFixed(3)}
-                            Mbps
-                          </List>
-                          <List>
-                            最大ビットレート:{" "}
-                            {(c.highBitrate / 1024).toFixed(3)}Mbps
-                          </List>
-                          <List>
-                            ビットレート: {(c.bitrate / 1024).toFixed(3)}Mbps
-                          </List>
-                          <List>
-                            総受信量:{" "}
-                            {(c.bytesReceived / 1024 / 1024).toFixed(3)}MB
-                          </List>
-                          <List>
-                            FPS: {c.framerate}fps (MAX: {c.highFramerate} / MIN:{" "}
-                            {c.lowFramerate})
-                          </List>
-                          <List>
-                            パケットロスト: {c.packetLost ? c.packetLost : 0}件
-                          </List>
-                          <List>RTT: {c.currentRoundTripTime}秒</List>
-                        </div>
-                      ) : null}
-                    </Paper>
-                  </Paper>
-                </Rnd>
-                <Rnd
-                  size={{ width: "600", height: "900" }}
-                  position={{ x: this.state.x3, y: this.state.y3 }}
-                  onDragStop={(e, d) => {
-                    this.setState({ x3: d.x, y3: d.y });
-                  }}
-                  onResizeStop={(e, direction, ref, delta, position) => {
-                    this.setState({
-                      width: ref.style.width,
-                      height: ref.style.height,
-                      ...position
-                    });
-                  }}
-                  enableResizing={{}}
-                >
-                  <Paper style={{ position: "relative" }}>
-                    <AppBar style={{ position: "relative", padding: 10 }}>
-                      パフォーマンス統計
-                    </AppBar>
-                    <Paper>
-                      <Line data={genData(this.state.aggregatedStats)} />
-                    </Paper>
-                  </Paper>
-                </Rnd>
-
-                {/* TODO: URL wo state nisuru */}
-                <Rnd
-                  size={{ width: "", height: "" }}
-                  position={{ x: this.state.x4, y: this.state.y4 }}
-                  onDragStop={(e, d) => {
-                    this.setState({ x4: d.x, y4: d.y });
-                  }}
-                  onResizeStop={(e, direction, ref, delta, position) => {
-                    this.setState({
-                      width: ref.style.width,
-                      height: ref.style.height,
-                      ...position
-                    });
-                  }}
-                  enableResizing={{}}
-                >
-                  <Paper style={{ position: "relative" }}>
-                    <AppBar style={{ position: "relative", padding: 10 }}>
-                      コンパネ
-                    </AppBar>
-                    <Paper>
-                      <Container>
-                        <FormControlLabel
-                          value="1280"
-                          control={
-                            <Radio
-                              color="primary"
-                              onChange={() => {
-                                emitter(
-                                  context.webRtcPlayerObj
-                                ).emitUIInteraction({
-                                  ConsoleCommand: "r.SetRes 1280x720"
-                                });
-                              }}
-                              checked={context.videoRes.width === 1280}
-                            />
-                          }
-                          label="1280"
-                          labelPlacement="top"
-                        />
-
-                        <FormControlLabel
-                          value="1920"
-                          control={
-                            <Radio
-                              color="primary"
-                              onChange={() => {
-                                emitter(
-                                  context.webRtcPlayerObj
-                                ).emitUIInteraction({
-                                  ConsoleCommand: "r.SetRes 1920x1080"
-                                });
-                              }}
-                              checked={context.videoRes.width === 1920}
-                            />
-                          }
-                          label="1920"
-                          labelPlacement="top"
-                        />
-
-                        <FormControlLabel
-                          value="2560"
-                          control={
-                            <Radio
-                              color="primary"
-                              onChange={() => {
-                                emitter(
-                                  context.webRtcPlayerObj
-                                ).emitUIInteraction({
-                                  ConsoleCommand: "r.SetRes 2560x1440"
-                                });
-                              }}
-                              checked={context.videoRes.width === 2560}
-                            />
-                          }
-                          label="2560"
-                          labelPlacement="top"
-                        />
-
-                        <FormControlLabel
-                          value="3840"
-                          control={
-                            <Radio
-                              color="primary"
-                              onChange={() => {
-                                emitter(
-                                  context.webRtcPlayerObj
-                                ).emitUIInteraction({
-                                  ConsoleCommand: "r.SetRes 3840x2160"
-                                });
-                              }}
-                              checked={context.videoRes.width === 3840}
-                            />
-                          }
-                          label="3840"
-                          labelPlacement="top"
-                        />
-                      </Container>
-                      <Container>
-                        <Typography id="discrete-slider-custom" gutterBottom>
-                          FPS制限
-                        </Typography>
-                        <Slider
-                          style={{ width: "200" }}
-                          defaultValue={60}
-                          getAriaValueText={valuetext}
-                          aria-labelledby="discrete-slider-custom"
-                          step={15}
-                          valueLabelDisplay="auto"
-                          marks={marks}
-                          min={30}
-                          max={180}
-                          onChangeCommitted={(e, val) => {
-                            console.log(val);
-                            emitter(context.webRtcPlayerObj).emitUIInteraction({
-                              ConsoleCommand: `t.maxFPS ${val}`
-                            });
-                          }}
-                        />
-                      </Container>
-                      <Container>
-                        <QRCode
-                          style={{ padding: "20px" }}
-                          value={window.location.href}
-                        />
-                      </Container>
-                    </Paper>
-                  </Paper>
-                </Rnd>
-              </div>
-            )}
-          </PixelStreamingContext.Consumer>
           {this.props.children}
-          {this.state.infomation ? (
-            <Rnd
-              size={{ width: "600", height: "900" }}
-              position={{ x: this.state.x5, y: this.state.y5 }}
-              onDragStop={(e, d) => {
-                this.setState({ x5: d.x, y5: d.y });
-              }}
-              onResizeStop={(e, direction, ref, delta, position) => {
-                this.setState({
-                  width: ref.style.width,
-                  height: ref.style.height,
-                  ...position
-                });
-              }}
-              enableResizing={{}}
-            >
-              <Paper style={{ position: "relative" }}>
-                <AppBar style={{ position: "relative" }}>適当な商品情報</AppBar>
-                <Paper style={{ padding: 30 }}>
-                  <List>商品名: {JSON.parse(this.state.infomation).name}</List>
-                  <List>{JSON.parse(this.state.infomation).colors}</List>
-                  <List>{JSON.parse(this.state.infomation).price}</List>
-                  <Button
-                    variant="contained"
-                    href="{JSON.parse(this.state.infomation).name}"
-                  >
-                    商品ページ
-                  </Button>
-
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={() => {
-                      this.setInfomation("");
-                    }}
-                  >
-                    Close
-                  </Button>
-                </Paper>
-              </Paper>
-            </Rnd>
-          ) : null}
         </div>
       </PixelStreamingContext.Provider>
     );
   }
 }
 
-function genData(arry) {
-  // return arry.map(data=>{
-  //   return {
-  //     time: data.timestamp - data.timestampStart/1000,
-  //     fps: data.framerate,
-  //     bitrate: data.bitrate,
-  //   }
-  // });
-  const labels = arry.map(elm =>
-    Math.round((elm.timestamp - elm.timestampStart) / 1000)
-  );
-
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "FPS",
-        type: "line",
-        data: arry.map(elm => elm.framerate),
-        borderColor: "rgba(54, 162, 235, 0.2)",
-        backgroundColor: "rgba(254,97,132,0.5)"
-      },
-      {
-        label: "Bitrate",
-        type: "line",
-        fill: false,
-        data: arry.map(elm => elm.bitrate),
-        borderColor: "rgba(254,97,132,0.8)",
-        backgroundColor: "rgba(254,97,132,0.5)"
-      },
-      {
-        label: "パケットロスト",
-        type: "line",
-        fill: false,
-        data: arry.map(elm => elm.packetsLost),
-        borderColor: "rgba(75, 192, 192, 0.2)",
-        backgroundColor: "rgba(254,97,132,0.5)"
-      }
-    ]
-  };
-  return data;
-}
-
-const marks = [
-  {
-    value: 30,
-    label: "30"
-  },
-  {
-    value: 60,
-    label: "60"
-  },
-  {
-    value: 90,
-    label: "90"
-  },
-  {
-    value: 120,
-    label: "120"
-  },
-  {
-    value: 150,
-    label: "150"
-  }
-];
-
-function valuetext(value) {
-  return `Limit: ${value}fps`;
-}
